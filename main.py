96959ba94d8114ad3d5713a4c0c2fd73c49c576a
Arteluff <mastertema2005@gmail.com>
2024-06-28 00:42:54 +0300

    Добавлена функция получения ссылок на вакансии

--- main.py
+++ main.py
@@ -4,26 +4,50 @@ from bs4 import BeautifulSoup
 ua = fake_useragent.UserAgent()
 req = input().replace(' ', '+')
 
-def openVacancies(inputText):
-    hhURL = f'https://hh.ru/search/vacancy?text={inputText}&items_on_page=100'
+def getLinksOfVacancies(inputText):
+    
     hhHeaders = {
         "user-agent": ua.random
     }
-    hhRequest = requests.get(hhURL, headers=hhHeaders)
-    hhSoup = BeautifulSoup(hhRequest.text, 'lxml')
-    vacancies = []
-    listOfSalaries = []
-    namesOfVacancies = hhSoup.findAll('span', {'class':'vacancy-name--c1Lay3KouCl7XasYakLk'})
-    salaries = hhSoup.findAll('span', {'class': 'compensation-text--kTJ0_rp54B2vNeZ3CTt2'})
+    hhRequest = requests.get(
+        url = f'https://hh.ru/search/vacancy?text={inputText}&items_on_page=100', 
+        headers = hhHeaders
+    )
 
-    for salary in salaries:
-        listOfSalaries.append(salary.text)
+    if hhRequest.status_code != 200:
+        return
+    
+    hhSoup = BeautifulSoup(hhRequest.content, 'lxml')
 
-    for nameOfVacancy in namesOfVacancies:
-        vacancies.append(nameOfVacancy.text)
+    hhPager = hhSoup.find('div', {'class': 'pager'})
+    if hhPager == None:
+        numberOfPages = 1
+    else:
+        numberOfPages = int(hhSoup.find('div', {'class': 'pager'}).find_all('span', recursive=False)[-1].find('a').find('span').text)
+    print(f'numberOfPages - {numberOfPages}')
+    count = numberOfPages
+    for page in range(numberOfPages):
+        print(f"Осталость страниц - {count}")
+        hhLinkRequest = requests.get(
+            url = f'https://hh.ru/search/vacancy?text={inputText}&items_on_page=100&page={page}', 
+            headers = hhHeaders
+        )
+        if hhLinkRequest.status_code != 200:
+            return
+        hhLinksHref = []
+        hhLinkSoup = BeautifulSoup(hhLinkRequest.content, 'lxml')
+        hhLinks = hhLinkSoup.find_all('a', {'class': 'bloko-link'})
+        for link in hhLinks:
+            if link.get('href') == None or (link.get('href')[0] == '/') or not ('vacancy' in link.get('href')):
+                continue
+            else:
+                hhHref = link.get('href')
+                hhLinksHref.append(hhHref)
+        # print(hhLinksHref)
+        count -= 1
+        # print(f'Всего вакансий на этой странице {len(hhLinksHref)}')
 
-    print(len(vacancies))
-    print(len(listOfSalaries))
+if __name__ == '__main__':
+    getLinksOfVacancies(req)
 
-print(openVacancies(req))
 
