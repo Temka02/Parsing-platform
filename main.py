8682ee867c760a31e5571038deee75098d0a3401
Arteluff <mastertema2005@gmail.com>
2024-06-28 22:17:35 +0300

    получение словарей с названием вакансии и зарплатой

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, fake_useragent, time, json, random
+import requests, fake_useragent, json, time, re
 from bs4 import BeautifulSoup
 
 ua = fake_useragent.UserAgent()
@@ -10,7 +10,7 @@ def getLinksOfVacancies(inputText):
         "user-agent": ua.random
     }
     hhRequest = requests.get(
-        url = f'https://hh.ru/search/vacancy?text={inputText}&items_on_page=100', 
+        url = f'https://hh.ru/search/vacancy?text={inputText}&items_on_page=20', 
         headers = hhHeaders
     )
 
@@ -26,28 +26,70 @@ def getLinksOfVacancies(inputText):
         numberOfPages = int(hhSoup.find('div', {'class': 'pager'}).find_all('span', recursive=False)[-1].find('a').find('span').text)
     print(f'numberOfPages - {numberOfPages}')
     count = numberOfPages
+
     for page in range(numberOfPages):
-        print(f"Осталость страниц - {count}")
-        hhLinkRequest = requests.get(
-            url = f'https://hh.ru/search/vacancy?text={inputText}&items_on_page=100&page={page}', 
-            headers = hhHeaders
-        )
-        if hhLinkRequest.status_code != 200:
-            return
-        hhLinksHref = []
-        hhLinkSoup = BeautifulSoup(hhLinkRequest.content, 'lxml')
-        hhLinks = hhLinkSoup.find_all('a', {'class': 'bloko-link'})
-        for link in hhLinks:
-            if link.get('href') == None or (link.get('href')[0] == '/') or not ('vacancy' in link.get('href')):
-                continue
-            else:
-                hhHref = link.get('href')
-                hhLinksHref.append(hhHref)
-        # print(hhLinksHref)
-        count -= 1
-        # print(f'Всего вакансий на этой странице {len(hhLinksHref)}')
+        try:
+            print(f"Осталость страниц - {count}")
+
+            hhLinkRequest = requests.get(
+                url = f'https://hh.ru/search/vacancy?text={inputText}&items_on_page=20&page={page}', 
+                headers = hhHeaders
+            )
+
+            if hhLinkRequest.status_code != 200:
+                return
+            
+            hhLinkSoup = BeautifulSoup(hhLinkRequest.content, 'lxml')
+            # hhLinks = hhLinkSoup.find_all('a', {'class': 'bloko-link'})
+            hhLinks = []
+            spans = hhLinkSoup.find_all('span', {'class': 'serp-item__title-link-wrapper'})
+            for span in spans:
+                hhLinks.append(span.find('a', {'class': 'bloko-link'}))
+
+
+            for link in hhLinks:
+                if link.get('href') == None or (link.get('href')[0] == '/') or not ('vacancy' in link.get('href')):
+                    continue
+                else:
+                    yield link.get('href')
+
+            count -= 1
+        except Exception as e:
+            print(f'{e}')
+        time.sleep(0.05)
+
+def getInfoFromVacancies(link):
+
+    hhHeaders = {
+        "user-agent": ua.random
+    }
+
+    hhRequest = requests.get(
+        url = link, 
+        headers = hhHeaders
+    )
+
+    if hhRequest.status_code != 200:
+        return
+    hhSoup = BeautifulSoup(hhRequest.content, 'lxml')
+    try:
+        curNameOfVacancy = hhSoup.find('div', {'class': 'vacancy-title'}).find('h1', {'class': 'bloko-header-section-1'}).text
+        curSalary = hhSoup.find('div', {'class': 'vacancy-title'}).find('span', {'class': 'magritte-text___pbpft_3-0-9'}).text
+    except:
+        curNameOfVacancy = 'Не найдена информация о названии вакансии'
+        curSalary = 'Не найдена информация о доходе'
+    vacancy = {
+        'name': curNameOfVacancy,
+        'salary': curSalary.replace('\xa0', '')
+    }
+    return vacancy   
+# magritte-text_style-primary___AQ7MW_3-0-9
 
 if __name__ == '__main__':
-    getLinksOfVacancies(req)
-
+    k = 1
+    for a in getLinksOfVacancies(req):
+        print(f'{k} - {getInfoFromVacancies(a)}')
+        k += 1
+        time.sleep(0.05)
+    
 
